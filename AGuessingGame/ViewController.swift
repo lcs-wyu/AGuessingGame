1b38d1d062ac52b9c1ec65626e766764a241a113
Wellesley Yu <byu@lcs.on.ca>
2019-10-28 02:43:22 -0400

    Got the comparison logic working

--- AGuessingGame/ViewController.swift
+++ AGuessingGame/ViewController.swift
@@ -49,11 +49,17 @@ class ViewController: UIViewController {
         let guessNumber = Int(guessText)!
         
         //For testing purposes what was the guess?
-        print("For testing purposes,the guess made was\(guessNumber)")
+        print("For testing purposes,the guess made was \(guessNumber)")
         
-        
-        
+        //give user appropriate feedback
+        if guessNumber > targetNumber {
+            print("Guess lower next time")
+        } else if  guessNumber < targetNumber {
+           print("Guess higher next time")
+        } else {
+        print("You are correct")
     }
     
 }
 
+}
